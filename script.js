4c5eb65310167d11746b22ff14a430376fbf4be9
ismaguilera <iaguiler@ug.uchile.cl>
2025-05-20 14:39:15 -0400

    actualización simple

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('HTML project loaded');
+    document.getElementById('year').textContent = new Date().getFullYear();
 });
-
-document.getElementById('year').textContent = new Date().getFullYear();
 
